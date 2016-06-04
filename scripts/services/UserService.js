fb763f85d0517f5a95458e6e128f9c33c347aa53
Abhijit <asonis@gmail.com>
2016-06-04 13:34:03 -0400

    added proxy setting

--- scripts/services/UserService.js
+++ scripts/services/UserService.js
@@ -1,15 +1,25 @@
 app.factory('UserService', ['$http', '$q', function($http, $q){ 
     return {         
-        getJson: function(firstName, lastName, age) {
-            return $http.jsonp("http://localhost:8080/baseball-league/team3/getJson?callback=JSON_CALLBACK&firstName="+firstName+"&lastName="+lastName+"&age="+age).
+        getJson: function(firstName, lastName, age) {        	
+        	params = {
+        			'firstName': firstName,
+        			'lastName': lastName,
+        			'age': age,
+        			'callback': 'JSON_CALLBACK'
+			};
+            return $http({
+            	url: '/baseball-league-web/getJson',
+            	method: 'JSONP',
+            	params: params
+			}).
             success(function(response) {
             	return response.data;
-              }).
-              error(function (response) {
-            	  var dd = JSON.stringify(response);
-            	  console.error('Error while fetching users');
-                  return $q.reject(dd);
-              });
+            }).
+            error(function (response) {
+            	var dd = JSON.stringify(response);
+            	console.error('Error while fetching users');
+                return $q.reject(dd);
+            });
         }         
     }; 
 }]);
