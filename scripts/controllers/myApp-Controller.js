d61e4a9c664a10ce3740dc1834348a77866d476f
Abhijit <asonis@gmail.com>
2016-07-05 13:39:29 -0400

    committed draft players

--- scripts/controllers/myApp-Controller.js
+++ scripts/controllers/myApp-Controller.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp', []);
 
 app.controller('myApp-Controller', ['$scope', 'UserService', function($scope, UserService) {
     $scope.myFunc = function () {
-    	UserService.getJson($scope.username, $scope.password, $scope.email).then(
+    	UserService.getJson($scope.username, $scope.password,$scope.passwordConf $scope.email).then(
 	        function(d) {
 	        	var response = d.data;
 	            $scope.fullName = response.username + " registered successfully";
@@ -12,4 +12,69 @@ app.controller('myApp-Controller', ['$scope', 'UserService', function($scope, Us
 	        }
         );    	
 	}
+    $scope.addPlayers = function() {
+		for(var i = $scope.unassignedPlayers.length - 1; i >= 0; i--){
+		    if($scope.unassignedPlayers[i].selected){
+		    	$scope.assignedPlayers.push({id:$scope.unassignedPlayers[i].id,name:$scope.unassignedPlayers[i].name});
+		        $scope.unassignedPlayers.splice(i,1);
+		    }
+		}
+    };
+    $scope.removePlayers = function(index) {
+    	for(var i = $scope.assignedPlayers.length - 1; i >= 0; i--){
+		    if($scope.assignedPlayers[i].selected){
+		    	$scope.unassignedPlayers.push({id:$scope.assignedPlayers[i].id,name:$scope.assignedPlayers[i].name});
+		        $scope.assignedPlayers.splice(i,1);
+		    }
+		}
+    };
+    $scope.getSeason = function() {
+    	$scope.seasons = [];
+    	$scope.seasons.push({
+    		id:'10000',name:'Summer',seasonstartdt:'',seasonenddt:'',seasonnumberofplayers:'18',leagueid:'10000'});
+    };
+    $scope.getDivision = function() {
+    	$scope.divisions = [];
+    	$scope.divisions.push({
+    		id:'10000',name:'Pony',divisionminage:'10',divisionmaxage:'14',divisionnumberofplayers:'18',seasonid:'10000'});
+    };
+    $scope.getTeams = function() {
+    	$scope.teams = [];
+    	$scope.teams.push({id:'10000',name:'Team1',teamnumberofplayers:'10',divisionid:'10000',fieldid:''},
+    					{id:'10001',name:'Team2',teamnumberofplayers:'10',divisionid:'10000',fieldid:''},
+    					{id:'10002',name:'Team3',teamnumberofplayers:'10',divisionid:'10000',fieldid:''});
+    };
+    /*angular.element(document).ready(function () {
+    	UserService.getJson().then(
+	        function(d) {
+	        	var response = d.data;
+	            alert()
+	        },
+	        function(errResponse){
+	        	console.error('Error while fetching Currencies');
+	        }
+        );
+    });*/
+    /*$scope.init = function (){
+    	UserService.getLeague().then(
+	        function(d) {
+	        	var response = d.data;
+	            $scope.fullName = response.username + " registered successfully";
+	        },
+	        function(errResponse){
+	        	console.error('Error while fetching Currencies');
+	        }
+        );
+    };*/
+   /* $scope.$on('$viewContentLoaded', function() {
+    	UserService.getLeague().then(
+    	        function(d) {
+    	        	var response = d.data;
+    	            $scope.fullName = response.username + " registered successfully";
+    	        },
+    	        function(errResponse){
+    	        	console.error('Error while fetching Currencies');
+    	        }
+            );
+    });*/
 }]);
--- scripts/services/UserService.js
+++ scripts/services/UserService.js
@@ -20,6 +20,49 @@ app.factory('UserService', ['$http', '$q', function($http, $q){
             	console.error('Error while fetching users');
                 return $q.reject(dd);
             });
+        }/*,
+        getLeague: function(){
+        	params = {
+        			'callback': 'JSON_CALLBACK'
+			};
+        	return $http({
+             	url: '/baseball-league-web/getLeague',
+             	method: 'JSONP',
+             	params: params
+ 			}).
+             success(function(response) {
+             	return response.data;
+             }).
+             error(function (response) {
+             	var dd = JSON.stringify(response);
+             	console.error('Error while fetching leagues');
+                 return $q.reject(dd);
+             });
+        }*/
+    }; 
+}]);
+
+app.factory('UserService', ['$http', '$q', function($http, $q){ 
+    return {         
+        getJson: function(leagueName, leagueLocation) {        	
+        	params = {
+        			'leagueName': leagueName,
+        			'leagueLocation': leagueLocation,
+        			'callback': 'JSON_CALLBACK'
+			};
+            return $http({
+            	url: '/baseball-league-web/getJson',
+            	method: 'JSONP',
+            	params: params
+			}).
+            success(function(response) {
+            	return response.data;
+            }).
+            error(function (response) {
+            	var dd = JSON.stringify(response);
+            	console.error('Error while creating league');
+                return $q.reject(dd);
+            });
         }         
     }; 
 }]);
