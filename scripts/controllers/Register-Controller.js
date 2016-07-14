ccb89bb72dcb922b111f870721ec51f6ade2c0f4
Brian <brian.p.hartel@gmail.com>
2016-07-13 21:33:09 -0700

    More Changes to Register Controller

--- scripts/controllers/Register-Controller.js
+++ scripts/controllers/Register-Controller.js
@@ -1,10 +1,10 @@
-app.controller('DraftPlayers-Controller', ['$scope', 'UserService', function($scope, UserService) {
-    $scope.submit = function() {
-    	UserService.AddPerson($scope.selectedTeam.id ,$scope.assignedPlayers).then(
-	        function(d) {
-	        	if(d.data === true)
-	        		alert("Players added successfully.");
-	        }
-        );
-    };
+app.controller('Register-Controller', ['$scope', 'UserService', function($scope, UserService) {
+    $scope.submit = function(){
+		for(var i = $scope.unassignedPlayers.length - 1; i >= 0; i--){
+		    if($scope.unassignedPlayers[i].selected){
+		    	$scope..push({id:$scope.unassignedPlayers[i].id,name:$scope.unassignedPlayers[i].name});
+		        $scope.unassignedPlayers.splice(i,1);
+		    }
+		}
+    }; 
 }]);
