63b33795d41d82eea36a686a5fc2d0a53f5c8743
asonis <abhijit.c.sonis@gmail.com>
2016-08-04 00:25:13 -0400

    refactored code

--- scripts/controllers/LeagueSubmit-Controller.js
+++ scripts/controllers/LeagueSubmit-Controller.js
@@ -1,7 +1,133 @@
-//var app = angular.module('myApp', []);
-
-
 app.controller('LeagueSubmit-Controller', ['$scope', 'UserService', '$window', '$location', function($scope, UserService, $window, $location) {
+	var map;
+	var mycenter = new google.maps.LatLng(40.7982173, -77.8620971);
+	var mapProp = {
+			center: mycenter,
+			zoom: 13,
+			mapTypeId: google.maps.MapTypeId.ROADMAP};
+	var request;
+	var service;
+	var placeInfo;
+	var geocoder;
+	var markers = [];
+	
+	var placename;
+	var confirmOK;
+	
+	$scope.initialize = function () {
+		map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+		request = {
+			location: mycenter,
+			radius: 8047,
+			types: ['park']
+			};
+		geocoder = new google.maps.Geocoder();
+		placeInfo = new google.maps.InfoWindow();
+	    service = new google.maps.places.PlacesService(map);
+		service.nearbySearch(request, $scope.callback);
+		
+		google.maps.event.addListener(map,'rightclick',function(event){
+			map.setCenter(event.latLng)
+			clearResults(markers)
+			var request = {
+					location: event.latLng,
+					radius: 8047,
+					types: ['park']
+					};
+			service.nearbySearch(request, $scope.callback());
+			})
+	};
+	
+	$scope.callback = function (results, status) {
+		
+		if(status == google.maps.places.PlacesServiceStatus.OK)
+		{
+			for(var i = 0; i < results.length; i++)
+			{
+				!$scope.createMarker(results[i]);
+				markers.push($scope.createMarker(results[i]));
+			}
+		}
+	};
+	
+	$scope.createMarker = function (place) {
+		var placeLoc = place.geometry.location;
+		var marker = new google.maps.Marker(
+			{
+				map: map,
+				position: place.geometry.location
+			});
+		
+		google.maps.event.addListener(marker,'click',function(){
+			placeInfo.setContent(place.name);
+			placeInfo.open(map,this);
+			placename = placeInfo.getContent(place.name);
+			
+		});
+		
+		
+		google.maps.event.addListener(marker,'dblclick',function(){
+			placeInfo.setContent(place.name);
+			placeInfo.open(map,this);
+			
+			placename = placeInfo.getContent(place.name);
+			confirmOK = confirm("Set your league location as:" +placename+ "?");
+			
+			if (confirmOK == true)
+				{
+				alert("You have selected:" +placename+ "league");
+				updateData(placename);
+				}
+			else
+				{
+				alert("Boohoo");
+				}	
+			
+			
+		});
+		    
+			
+		return marker;
+	};
+	$scope.updateData = function (placename) {
+		
+	    alert("InsideUpdateData" +placename);
+	    submitleague(placename);	
+	};
+	
+	$scope.clearResults = function (markers) {
+		for (var m in markers)
+		{
+			markers[m].setMap(null)
+		}
+		markers = []
+	};
+	
+	$scope.codeAddress = function () {
+		var address = document.getElementById('address').value;
+		geocoder.geocode({'address': address}, function(results,status) 
+		{
+			if(status == google.maps.GeocoderStatus.OK)
+			{
+				map.setCenter(results[0].geometry.location);
+				var marker = new google.maps.Marker({
+					map: map,
+					position: results[0].geometry.location});
+					clearResults(markers)
+				var request = {
+						location: results[0].geometry.location,
+						radius: 8047,
+						types: ['park']
+						};
+				service.nearbySearch(request, callback);
+			}
+			else
+			{
+				alert('Geocode was not successful for the following reason: ' + status);
+			}
+		});
+	};
+	
     $scope.leagueLocation = $window.placename;
     $scope.currentState = 'Select';
     //$scope.leagues = ["League1", "League2", "League3"];
@@ -36,4 +162,5 @@ app.controller('LeagueSubmit-Controller', ['$scope', 'UserService', '$window', '
 	        }
         );
 	};
+	$scope.initialize();
 }]);
